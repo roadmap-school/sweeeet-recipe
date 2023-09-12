302ee987198b7f2f25c102099157512d01df7ae6
Antoinelacombled <antoine.j.lacombled@gmail.com>
2023-09-12 15:02:26 +0200

    using route component to select the good page

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,4 +1,5 @@
 import Recipe from "../Recipe/Recipe";
+import { Route, Routes } from "react-router-dom";
 
 import './page.scss';
 import Home from "../Home/Home";
@@ -9,12 +10,26 @@ const Page = () => (
     <div className="page" >
         <h1 className="page-title">Orecipe</h1>
         <div className="page-content">
-            {/* home or recipe >> */}
-            {/* <Recipe recipe={data[0]} /> */}
-            <Home />
+
+
+            <Routes>
+                {/* 1st route */}
+
+                <Route
+                    path="/"
+                    element={<Home />} />
+
+                {/* 2nd route */}
+
+                <Route
+                    path="/recipe/recette1"
+                    element={<Recipe recipe={data[0]} />} />
+
+
+            </Routes>
+
         </div>
     </div>
-
 );
 
 export default Page; 
