a624cd83b1f2d1648594ceee7a2c6af932dd6a46
Antoinelacombled <antoine.j.lacombled@gmail.com>
2023-09-15 18:36:59 +0200

    display ok

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -3,6 +3,8 @@ import { combineReducers } from "redux";
 // on importe les reducers à combiner : 
 import recipesReducer from "./recipes";
 
+console.log(listRecipes);
+
 const rootReducer = combineReducers({
     // étiquette : reducer
     recipes: recipesReducer, 
